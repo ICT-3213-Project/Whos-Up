f8d7586e7f0c9dcbeba3e9f5ef9c5131393ad659
Vinayak Agarwal <vinayakagarwal6996@gmail.com>
2017-03-29 18:33:37 +0530

    Link activities

--- app/src/main/java/lightning/structby/whosup/MapsActivity.java
+++ app/src/main/java/lightning/structby/whosup/MapsActivity.java
@@ -1,9 +1,11 @@
 package lightning.structby.whosup;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -28,16 +30,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
@@ -56,5 +48,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
         // Position the map's camera near Sydney, Australia.
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(13.3453188, 74.7952696), 16));
+    }
+
+    public void eventDetails(View v){
+        Intent i = new Intent(this, eventDetails.class);
+        startActivity(i);
     }
 }
