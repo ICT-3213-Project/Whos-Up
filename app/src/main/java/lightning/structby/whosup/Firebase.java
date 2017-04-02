44cdaa299bd4c6295d8bb57217e599ab7ce243ad
Vinayak Agarwal <vinayakagarwal6996@gmail.com>
2017-04-02 09:46:35 +0530

    Chat stuff

---
+++ app/src/main/java/lightning/structby/whosup/Firebase.java
@@ -0,0 +1,27 @@
+package lightning.structby.whosup;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by vinayak on 4/1/17.
+ */
+
+public class Firebase extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // TODO: Implement this method to send any registration to your app's servers.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+
+}
