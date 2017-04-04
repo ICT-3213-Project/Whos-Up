02091576a6942b187eb104f400ed447dd1805c4a
Ujjwal Arora <arora.ujjwal@gmail.com>
2017-04-05 01:59:43 +0530

    Load profile

--- app/src/main/java/lightning/structby/whosup/ProfileActivity.java
+++ app/src/main/java/lightning/structby/whosup/ProfileActivity.java
@@ -4,22 +4,57 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    EditText name, shortBio, about;
+    EditText name, shortBio;
+    com.makeramen.roundedimageview.RoundedImageView profileImage;
+
+    FirebaseDatabase database;
+    DatabaseReference userRef;
+    FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        database = FirebaseDatabase.getInstance();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        userRef = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+
+
+        userRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                loadProfile(user);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Failed to read value
+
+                Toast.makeText(ProfileActivity.this, "Failed to load profile", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
         name = (EditText) findViewById(R.id.user_profile_name);
         shortBio = (EditText) findViewById(R.id.user_profile_short_bio);
-        about = (EditText) findViewById(R.id.user_profile_about);
-
-        loadProfile();
+        profileImage = (com.makeramen.roundedimageview.RoundedImageView) findViewById(R.id.user_profile_image);
 
         name.addTextChangedListener(new TextWatcher() {
             @Override
@@ -55,26 +90,12 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
-        about.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
     }
 
-    private void loadProfile() {
-
+    private void loadProfile(User user) {
+        name.setText(user.getName());
+        shortBio.setText(user.getShortBio());
+        //TODO: Profile
     }
 
 
