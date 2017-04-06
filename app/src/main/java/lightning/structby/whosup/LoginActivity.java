098393eb05bdf8a000de5876a61fc55f792b9f05
Vinayak Agarwal <vinayakagarwal6996@gmail.com>
2017-04-06 17:07:29 +0530

    Improve Login Page UI

--- app/src/main/java/lightning/structby/whosup/LoginActivity.java
+++ app/src/main/java/lightning/structby/whosup/LoginActivity.java
@@ -34,8 +34,8 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Intent i = new Intent(this, EventDetailsActivity.class);
-        startActivity(i);
+//        Intent i = new Intent(this, EventDetailsActivity.class);
+//        startActivity(i);
         mAuth = FirebaseAuth.getInstance();
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
